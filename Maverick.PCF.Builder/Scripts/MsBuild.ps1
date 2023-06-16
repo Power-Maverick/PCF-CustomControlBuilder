164159fa18a411444a79307f1dcea287de87c058
Danish-Mentorange <danish@mentorange.com>
2023-06-16 11:05:38 -0400

    Update to 2022 msbuild path

--- Maverick.PCF.Builder/Scripts/MsBuild.ps1
+++ Maverick.PCF.Builder/Scripts/MsBuild.ps1
@@ -1,5 +1,9 @@
-﻿Function Find-MsBuild([int] $MaxVersion = 2019)
+﻿Function Find-MsBuild([int] $MaxVersion = 2022)
 {
+    $agentPath2022 = "$Env:programfiles\Microsoft Visual Studio\2022\BuildTools\MSBuild\Current\Bin\"
+    $devPath2022 = "$Env:programfiles\Microsoft Visual Studio\2022\Enterprise\MSBuild\Current\Bin\"
+    $proPath2022 = "$Env:programfiles\Microsoft Visual Studio\2022\Professional\MSBuild\Current\Bin\"
+    $communityPath2022 = "$Env:programfiles\Microsoft Visual Studio\2022\Community\MSBuild\Current\Bin\"
     $agentPath2019 = "$Env:programfiles (x86)\Microsoft Visual Studio\2019\BuildTools\MSBuild\Current\Bin\"
     $devPath2019 = "$Env:programfiles (x86)\Microsoft Visual Studio\2019\Enterprise\MSBuild\Current\Bin\"
     $proPath2019 = "$Env:programfiles (x86)\Microsoft Visual Studio\2019\Professional\MSBuild\Current\Bin\"
@@ -12,6 +16,10 @@
     $fallback2013Path = "${Env:ProgramFiles(x86)}\MSBuild\12.0\Bin\"
     $fallbackPath = "C:\Windows\Microsoft.NET\Framework\v4.0.30319"
 		
+    If ((2022 -le $MaxVersion) -And (Test-Path $agentPath2022)) { return $agentPath2022 } 
+    If ((2022 -le $MaxVersion) -And (Test-Path $devPath2022)) { return $devPath2022 } 
+    If ((2022 -le $MaxVersion) -And (Test-Path $proPath2022)) { return $proPath2022} 
+    If ((2022 -le $MaxVersion) -And (Test-Path $communityPath2022)) { return $communityPath2022 } 
     If ((2019 -le $MaxVersion) -And (Test-Path $agentPath2019)) { return $agentPath2019 } 
     If ((2019 -le $MaxVersion) -And (Test-Path $devPath2019)) { return $devPath2019 } 
     If ((2019 -le $MaxVersion) -And (Test-Path $proPath2019)) { return $proPath2019 } 
